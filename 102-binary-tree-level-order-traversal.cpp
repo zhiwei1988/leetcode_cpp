0562d712b601b6d651399871604e00e2786b3772
zhiwei1988 <zhiweix1988@gmail.com>
2022-01-02 20:41:57 +0800

    solve problem 102

---
+++ 102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,40 @@
+//
+// Created by ZhiWei Tan on 1/2/22.
+//
+
+#include "playgroud.h"
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> tQueue;
+        vector<vector<int>> res;
+
+        if (root == nullptr) {
+            return res;
+        }
+
+        tQueue.push(root);
+        while (!tQueue.empty()) {
+            int size = tQueue.size();
+            vector<int> levelRes;
+            for (int i = 0; i < size; i++) {
+                TreeNode *node = tQueue.front();
+                tQueue.pop();
+                levelRes.push_back(node->val);
+
+                if (node->left != nullptr) {
+                    tQueue.push(node->left);
+                }
+
+                if (node->right != nullptr) {
+                    tQueue.push(node->right);
+                }
+            }
+            res.push_back(levelRes);
+        }
+
+        return res;
+    }
+};
+
--- playgroud.h
+++ playgroud.h
@@ -25,6 +25,16 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 string integerVectorToString(vector<int> list, int length = -1);
 vector<int> stringToIntegerVector(string input);
 ListNode* stringToListNode(string input);
