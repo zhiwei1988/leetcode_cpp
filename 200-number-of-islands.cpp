b943e2e51136969e10fc4b1824ec855a00424b8e
zhiwei1988 <zhiweix1988@gmail.com>
2022-01-02 21:01:04 +0800

    solve problem 200

---
+++ 200-number-of-islands.cpp
@@ -0,0 +1,65 @@
+//
+// Created by ZhiWei Tan on 1/2/22.
+//
+
+#include "playgroud.h"
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visted(m, vector<bool>(n, false));
+        int islandNums = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && visted[i][j] == false) {
+                    dfs(grid, visted, i, j, m, n);
+                    islandNums++;
+                }
+            }
+        }
+
+        return islandNums;
+    }
+
+private:
+    void dfs(vector<vector<char>> &grid, vector<vector<bool>> &visted,
+             int i, int j, int m, int n)
+    {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+
+        if (visted[i][j] == true) {
+            return;
+        }
+
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        visted[i][j] = true;
+
+        dfs(grid, visted, i+1, j, m, n);
+        dfs(grid, visted, i-1, j, m, n);
+        dfs(grid, visted, i, j+1, m, n);
+        dfs(grid, visted, i, j-1, m, n);
+    }
+};
+
+int main()
+{
+    vector<vector<char>> grid = {
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '0', '0'}
+    };
+
+    auto ret = Solution().numIslands(grid);
+    cout << "ans = " << ret << endl;
+    return 0;
+}
+
