91c6c38d42d31f7c229f0552a9f63dcb808f8819
zhiwei1988 <zhiweix1988@gmail.com>
2022-02-02 12:14:08 +0800

    solve problem 1425

---
+++ 1425-constrained-subsequence-sum.cpp
@@ -0,0 +1,47 @@
+//
+// Created by ZhiWei Tan on 2/2/22.
+//
+
+#include "playground.h"
+
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        // sum[i] 表示以 index=i 的元素作为结尾的子序列的和的最大值
+        vector<int> sum(nums.size());
+        deque<int> q; // 队列里保存sum元素下标，元素值单调递减
+        int res = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            sum[i] = nums[i];
+            while (!q.empty() && i - q.front() > k) {
+                q.pop_front(); // 队列首元素不符合要求
+            }
+
+            if (!q.empty()) {
+                // sum[i-1] 到 sum[i-k] 代表的子序列都可以将 nums[i] 添加进来
+                // 取其中的最大值
+                sum[i] += sum[q.front()];
+            }
+
+            res = max(res, sum[i]);
+
+            while (!q.empty() && sum[i] >= sum[q.back()]) {
+                q.pop_back(); // 保证队列单调递减的性质
+            }
+
+            if (sum[i] > 0) {
+                q.push_back(i);
+            }
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {10,2,-10,5,20};
+    auto ret = Solution().constrainedSubsetSum(nums, 2);
+    return 0;
+}
+
